51c4df932af4dc38b6d97855a79d33f09e9be34f
ArtHome12 <_mag12@yahoo.com>
2020-06-14 16:14:00 +0700

    +

--- src/callback.rs
+++ src/callback.rs
@@ -25,7 +25,7 @@ impl OrdersCommand {
       match input.get(..3).unwrap_or_default() {
          "bas" => OrdersCommand::Basket(1, 1, 1),
          "add" => OrdersCommand::Add(1, 1, 1),
-         "del" => OrdersCommand::Remove(1, 1, 1),
+         "1del" => OrdersCommand::Remove(1, 1, 1),
          _ => OrdersCommand::UnknownCommand,
      }
    }
@@ -37,25 +37,25 @@ pub async fn handle_message(cx: DispatcherHandlerCx<CallbackQuery>) {
    // Сообщение для отправки обратно
    let msg = match query.data {
       None => {
-         "Error handle_message None"
+         String::from("Error handle_message None")
       }
       Some(data) => {
          // Идентифицируем и исполним команду
          match OrdersCommand::from(&data) {
-            OrdersCommand::Basket(_rest_num, _group_num, _dish_num) => "В корзину",
-            OrdersCommand::UnknownCommand => "Error handle_message Some",
-            OrdersCommand::Add(_rest_num, _group_num, _dish_num) => "Добавить",
-            OrdersCommand::Remove(_rest_num, _group_num, _dish_num) => "Удалить",
+            OrdersCommand::Basket(_rest_num, _group_num, _dish_num) => String::from("В корзину"),
+            OrdersCommand::UnknownCommand => format!("Error handle_message {}", &data),
+            OrdersCommand::Add(_rest_num, _group_num, _dish_num) => String::from("Добавить"),
+            OrdersCommand::Remove(_rest_num, _group_num, _dish_num) => String::from("Удалить"),
          }
       }
    };
 
    // Отправляем ответ
    match cx.bot.answer_callback_query(query.id)
-      .text(msg)
+      .text(&msg)
       .send()
       .await {
-      Err(_) => log::info!("Error handle_message {}", msg),
+      Err(_) => log::info!("Error handle_message {}", &msg),
       _ => (),
    }
 }
