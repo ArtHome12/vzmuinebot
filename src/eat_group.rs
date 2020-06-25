c44171b136c3fedb638740f9e15a449c736b6e86
Артем <_mag12@yahoo.com>
2020-06-25 09:16:57 +0700

    +

--- src/eat_group.rs
+++ src/eat_group.rs
@@ -183,6 +183,7 @@ pub async fn show_inline_interface(cx: &DispatcherHandlerCx<CallbackQuery>, rest
          let mut buttons: Vec<InlineKeyboardButton> = info.groups.into_iter()
          .map(|(key, value)| (InlineKeyboardButton::callback(value, format!("drg{}", db::make_key_3_int(rest_num, key, cat_id)))))
          .collect();
+         db::log(&format!("drg{}", db::make_key_3_int(rest_num, 99, cat_id))).await;
 
          // Последняя непарная кнопка, если есть
          let last = if buttons.len() % 2 == 1 { buttons.pop() } else { None };
