fbf1bd282919a10449a34182734cc793b9f85878
Артем <_mag12@yahoo.com>
2020-06-26 07:34:49 +0700

    +

--- src/commands.rs
+++ src/commands.rs
@@ -217,11 +217,12 @@ where
 //
 pub async fn send_photo(cx: &Cx<()>, text: &str, markup: ReplyMarkup, image_id : String) 
 {
-   log::info!("Here no markup image={}", image_id);
+   log::info!("Here no image image={}", image_id);
    // Отправляем картинку и текст как комментарий
-   let res = cx.answer_photo(InputFile::file_id(image_id))
-   .caption(text)
-   // .reply_markup(markup)
+   // let res = cx.answer_photo(InputFile::file_id(image_id))
+   // .caption(text)
+   let res = cx.answer(text)
+   .reply_markup(markup)
    .disable_notification(true)
    .send()
    .await;
