48ce063fbab6562ae0314719408de854a4ddd02e
Артем <_mag12@yahoo.com>
2020-07-31 16:33:58 +0700

    +

--- src/database.rs
+++ src/database.rs
@@ -655,7 +655,7 @@ pub async fn dish_list(by: DishesBy) -> Option<DishList> {
          "SELECT DISTINCT d.rest_num, d.dish_num, d.title, d.info, d.active, d.group_num, d.price, d.image_id FROM dishes as d
          INNER JOIN restaurants r ON r.rest_num = d.rest_num
          INNER JOIN groups g ON g.group_num = d.group_num
-         WHERE r.active = TRUE AND r.enabled = TRUE AND g.active = TRUE AND UPPER(d.title) like UPPER($1::VARCHAR(100)) ORDER BY r.rest_num, g.group_num",
+         WHERE r.active = TRUE AND r.enabled = TRUE AND g.active = TRUE AND UPPER(d.title) like UPPER($1::VARCHAR(100)) ORDER BY d.rest_num, d.group_num",
    };
 
    // Подготовим нужный запрос с кешем благодаря пулу
