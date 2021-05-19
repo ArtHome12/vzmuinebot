1e63950e0cba12a8fd52d2f10f2506a0fae860a4
Артем <_mag12@yahoo.com>
2021-05-19 14:35:38 +0700

    First node insert

--- src/gear.rs
+++ src/gear.rs
@@ -15,51 +15,93 @@ use crate::states::*;
 use crate::database as db;
 use crate::node::Node;
 
+enum Command {
+   Add, // add a new node
+   Exit,  // return to start menu
+   Unknown,
+}
+
+impl From<&str> for Command {
+   fn from(s: &str) -> Command {
+      match s {
+         "/Add" => Command::Add,
+         "В начало" => Command::Exit,
+         _ => Command::Unknown,
+      }
+   }
+}
+
+impl From<Command> for String {
+   fn from(c: Command) -> String {
+      match c {
+         Command::Add => String::from("/Add"),
+         Command::Exit => String::from("В начало"),
+         Command::Unknown => String::from("Неизвестная команда"),
+      }
+   }
+}
+
+
 pub struct GearState {
    pub state: CommandState,
+   node: Option<Node>, // current displaying node
 }
+
+fn map_req_err(s: String) -> RequestError {
+   RequestError::ApiError{
+      kind: ApiError::Unknown(s), 
+      status_code: StatusCode::OK,
+   }
+}
+
 
 #[teloxide(subtransition)]
 async fn gear(state: GearState, cx: TransitionIn<AutoSend<Bot>>, ans: String,) -> TransitionOut<Dialogue> {
-   match ans.as_str() {
-      "/Add" => {
-         // Add a new child node
+   
+   // Parse and handle commands
+   match Command::from(ans.as_str()) {
+      Command::Add => {
+         // Store a new child node to database
+         let parent_id = state.node.map_or(0, |n| n.id);
+         let node = Node::new(parent_id);
+         db::insert_node(&node)
+         .await
+         .map_err(|s| map_req_err(s))?;
 
-         // Reload node
-         return enter(state.state, cx).await;
+         // Reload current node
+         enter(state.state, cx).await
       }
-      "В начало" => {
-         // Return to waiting for commands of the main menu
-         return crate::states::enter(StartState { restarted: false }, cx, ans).await;
-      }
-      _ => {
-         cx.answer("Неизвестная команда, вы находитесь в меню настроек")
-         .await?;
-      }
-   };
 
-   // Stay in place
-   next(state)
+      Command::Exit => crate::states::enter(StartState { restarted: false }, cx, ans).await,
+
+      Command::Unknown => {
+         cx.answer(format!("Неизвестная команда '{}', вы находитесь в меню настроек", ans)).await?;
+
+         // Stay in place
+         next(state)
+      }
+   }
 }
 
 pub async fn enter(state: CommandState, cx: TransitionIn<AutoSend<Bot>>,) -> TransitionOut<Dialogue> {
 
    let (node, info) = if state.is_admin {
       // Create root node
-      let node = Node::new();
+      let node = Node::new(0);
 
       // Load children
       let node = db::node(db::LoadNode::Children(node));
 
-      (node, "Записи:\n/Add Добавить")
+      (node, format!("Записи:\n{} Добавить", String::from(Command::Add)))
    } else {
-      (db::node(db::LoadNode::Owner(state.user_id)), "Нет доступных настроек")
+      (db::node(db::LoadNode::Owner(state.user_id)), String::from("Нет доступных настроек"))
    };
-   node.await.map_err(|op| RequestError::ApiError{kind: ApiError::Unknown(op), status_code: StatusCode::OK})?;
+   let node = node.await
+   .map_err(|s| map_req_err(s))?;
 
    cx.answer(info)
-   .reply_markup(one_button_markup("В начало"))
+   .reply_markup(one_button_markup(String::from(Command::Exit)))
    .await?;
 
-   next(GearState { state })
+   next(GearState { state, node: Some(node) })
 }
