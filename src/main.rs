2809557ddbe18a4ceaf7c9a55b7e04ed828601a8
Артем <_mag12@yahoo.com>
2022-07-27 08:14:13 +0700

    +

--- src/main.rs
+++ src/main.rs
@@ -124,8 +124,12 @@ where
 {
     fn handle_error(self: Arc<Self>, error: E) -> BoxFuture<'static, ()> {
       let text = format!("main::An error has occurred in the dispatcher:{:?}", error);
-      futures::executor::block_on(async {environment::log(&text).await});
+      // futures::executor::block_on(async {environment::log(&text).await});
       log::error!("here1{}", text);
+
+      tokio::spawn(async move {
+         Box::pin(async move {environment::log(&text).await});
+      });
 
       Box::pin(async {})
    }
