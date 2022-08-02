6b37acadac97138de0c85b1483bf972142d985d3
Артем <_mag12@yahoo.com>
2022-08-02 08:02:51 +0700

    +

--- src/main.rs
+++ src/main.rs
@@ -136,8 +136,9 @@ where
 
 async fn run() {
    // pretty_env_logger::init();
-   let mut builder = pretty_env_logger::formatted_builder();
+   let mut builder = env_logger::Builder::from_default_env();
    builder.target(env_logger::Target::Stdout);
+
    builder.init();
 
    log::info!("Starting...");
